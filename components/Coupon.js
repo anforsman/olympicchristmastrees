95f1bdf04bd44c2c5cd15857980c725c0e796d6b
sycamoredocs <47806995+sycamoredocs@users.noreply.github.com>
2020-11-25 20:30:15 -0800

    netlify forms for Coupon form

--- components/Coupon.js
+++ components/Coupon.js
@@ -13,8 +13,16 @@ const Coupon = () => {
                 </p>
                 </div>
                 <div className="mt-8 lg:mt-0 lg:ml-8">
-                    <form className="sm:flex" aria-labelledby="newsletter-headline"
-                    id="mc-embedded-subscribe-form" action="http://olympictrees.us6.list-manage1.com/subscribe/post?u=5ab7bc9cf312e2edb49ea965d&amp;id=4e98db6942" method="post" name="mc-embedded-subscribe-form" novalidate="" target="_blank" >
+                    <form
+                        className="sm:flex" aria-labelledby="newsletter-headline"
+                        id="mc-embedded-subscribe-form"
+                        method="post"
+                        name="mc-embedded-subscribe-form"
+                        novalidate=""
+                        target="_blank"
+                        data-netlify="true"
+                        >
+                            <input type="hidden" name="form-name" value="mc-embedded-subscribe-form" />
                         <input aria-label="Email address" type="email" required className="appearance-none w-full px-5 py-3 border border-transparent text-base leading-6 rounded-md text-gray-900 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 transition duration-150 ease-in-out sm:max-w-xs" placeholder="Enter your email" />
                         <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
                         <button className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-red-700 hover:bg-red-600 focus:outline-none focus:bg-red-400 transition duration-150 ease-in-out">

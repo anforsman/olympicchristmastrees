e5a9fb99ca0f0c16fe90dc96bf08a4369bddd3ba
Andrew Forsman <anforsman@users.noreply.github.com>
2021-12-14 23:53:48 -0800

    Update Location.js

--- components/Location.js
+++ components/Location.js
@@ -14,7 +14,7 @@ const Location = () => {
                 Archibald &amp; Baseline
                 </h1>
                 <p className="mt-3 pb-4 text-lg leading-6 text-gray-700">
-                    The tree lot opens November 26th! 2021 marks our 42nd year selling trees in the Inland Empire.
+                    The tree lot is sold out. We will see you again in 2022!
                 </p>
 
                 <div className="pb-4">
@@ -29,8 +29,7 @@ const Location = () => {
 
                 <h3 className="block text-gray-700 font-bold uppercase text-xs">Hours</h3>
                 <ul>
-                    <li>Fri - Sat: 9:00a - 9:00p</li>
-                    <li>Sun - Thurs: 10:00a - 8:00p</li>
+                    <li>Closed for the 2021 season.</li>
                 </ul>
             </div>
 
